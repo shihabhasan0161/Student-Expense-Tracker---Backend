06a18e2c47dfb445d047e76c52641849bed116d2
shihab <shihabhasan0161@gmail.com>
2025-09-01 13:57:08 -0400

    initial folder

---
+++ studentexpensetracker/src/main/java/com/studentexpensetracker/studentexpensetracker/StudentexpensetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.studentexpensetracker.studentexpensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentexpensetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentexpensetrackerApplication.class, args);
+	}
+
+}
---
+++ studentexpensetracker/src/test/java/com/studentexpensetracker/studentexpensetracker/StudentexpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentexpensetracker.studentexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentexpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
